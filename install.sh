7ec35ca27d32f435e584533e9e2386f5f0508502
magasword22 <magasword22universe@gmail.com>
2026-05-15 16:57:35 +0200

    fix(install): Quote setuptools to prevent bash redirection error

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ echo "[*] Étape 6 : Configuration de l'environnement Python..."
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install setuptools<70
+pip install "setuptools<70"
 pip install -r requirements.txt
 
 # Création d'un exécutable CLI (Gestionnaire)
